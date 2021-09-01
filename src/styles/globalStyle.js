d509e21cd518eb767d3733e9183dbc63663469ed
chimdie <nzechimdi@yahoo.com>
2021-09-01 21:16:55 +0100

    feat/themes

---
+++ src/styles/globalStyle.js
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from "styled-components";
+
+export const darkTheme = {
+  body: "#000000",
+  color: "#6B7280",
+};
+
+export const lightTheme = {
+  body: "#f5f5f5",
+  color: "#000000",
+};
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body{
+    height: 100%;
+    font-family: 'Sniglet', cursive, 'Roboto', 'Oxygen';
+    /* color: rgba(107, 114, 128); */
+    color: ${({ theme }) => theme.color};
+    background-color: ${({ theme }) => theme.body};
+    line-height: 1.5;
+    transition: background-color 0.2s ease-in, color 0.2s ease-in;
+  }
+`;
+
+// export default GlobalStyle;
---
+++ src/styles/theme.js
@@ -0,0 +1,9 @@
+export const darkTheme = {
+  body: "#000000",
+  fontColor: "#fff",
+};
+
+export const lightTheme = {
+  body: "#ffffff",
+  fontColor: "#000000",
+};
